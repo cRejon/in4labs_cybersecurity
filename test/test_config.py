3d76217ba8a37360b936496a886b26f092381860
cRejon <carlosrejongomez@gmail.com>
2025-01-21 18:23:11 +0100

    Fix test config

--- test/test_config.py
+++ test/test_config.py
@@ -1,13 +1,13 @@
 class Config(object):
     # Labs settings
     labs_config = {
-        'duration': 15, # minutes
+        'duration': 20, # minutes
         'labs': [{
-            'lab_name' : 'in4labs_perception',
-            'html_name' : 'Perception Laboratory',
-            'description' : 'This lab performs sensors experiments on Arduino devices',
-            'host_port' : 8001,
+            'lab_name' : 'in4labs_cybersecurity',
+            'html_name' : 'Laboratory of Cybersecurity',
+            'description' : 'This lab performs cybersecurity experiments on Arduino devices.',
+            'host_port' : 8002,
             'nat_port' : 8120,
-            'cam_url': 'http://62.204.201.51:8100/Mjpeg/1?authToken=2454ef16-84cf-4184-a748-8bddd993c078',
+            'cam_url': '',
         }],
     }
